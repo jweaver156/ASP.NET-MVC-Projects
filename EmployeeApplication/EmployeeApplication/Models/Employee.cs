fec288615a752a5782ddec5659462d0def5dec94
jweaver156 <jweaver156@gmail.com>
2020-07-02 16:57:11 -0700

    entity code first

---
+++ EmployeeApplication/EmployeeApplication/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApplication.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "This field is mandatory")]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "This field is mandatory")]
+        public string PhoneNumber { get; set; }
+        public string Designation { get; set; }
+        public string Department { set; get; }
+    }
+}
---
+++ EmployeeApplication/EmployeeApplication/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EmployeeApplication.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("EmployeeDB")
+        { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
